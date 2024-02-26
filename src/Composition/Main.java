fa58931734484bdb64f57a3d49df8265f8510389
GIL32610 <gil.00000032610@unicap.br>
2024-02-26 16:27:13 -0300

    Composition and relationships

---
+++ src/Composition/Main.java
@@ -0,0 +1,7 @@
+package Composition;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Composition/PersonalComputer.java
@@ -0,0 +1,29 @@
+package Composition;
+
+public class PersonalComputer extends Product{
+
+    //Composition is a HAS A relationship
+    //alternative to inheritance
+    private ComputerCase computerCase;
+    private Monitor monitor;
+    private MotherBoard motherBoard;
+
+    public PersonalComputer(String model, String manufacturer, ComputerCase computerCase, Monitor monitor, MotherBoard motherBoard) {
+        super(model, manufacturer);
+        this.computerCase = computerCase;
+        this.monitor = monitor;
+        this.motherBoard = motherBoard;
+    }
+
+    public ComputerCase getComputerCase() {
+        return computerCase;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public MotherBoard getMotherBoard() {
+        return motherBoard;
+    }
+}
