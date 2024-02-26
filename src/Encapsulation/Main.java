cdc809297c1029e334c4e3e43c93a059f1376936
GIL32610 <gil.00000032610@unicap.br>
2024-02-26 18:02:54 -0300

    Problems caused by not using Encapsulation

---
+++ src/Encapsulation/Main.java
@@ -0,0 +1,18 @@
+package Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+//
+//        Player player = new Player();
+//        player.name = "Gil";
+//        player.health = 30;
+//        player.weapon = "Machine Gun";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+//        player.health = 499;
+//        player.loseHealth(21);
+//        System.out.println("Remaining health = " + player.healthRemaining());
+    }
+}
---
+++ src/Encapsulation/Player.java
@@ -0,0 +1,27 @@
+package Encapsulation;
+
+public class Player {
+
+    public String fullName;
+    public int health;
+    public String weapon;
+
+    public void loseHealth(int damage) {
+        health -= damage;
+        if (health <= 0) {
+            System.out.println("Player knocked out");
+        }
+    }
+
+    public int healthRemaining() {
+        return health;
+    }
+
+    public void restoreHealth(int extraHealth) {
+        health += extraHealth;
+        if (health > 100) {
+            System.out.println("Player restored to 100%");
+            health = 100;
+        }
+    }
+}
