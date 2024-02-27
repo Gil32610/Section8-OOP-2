955bc19ca72a7992eb940a4752049fae627e1881
GIL32610 <gil.00000032610@unicap.br>
2024-02-27 17:15:39 -0300

    Encapsulation Challenge Correction

--- src/Encapsulation/EncapsulationChallenge/Main.java
+++ src/Encapsulation/EncapsulationChallenge/Main.java
@@ -8,6 +8,15 @@ public class Main {
         System.out.println("Printing " + printer.printPages(23) + " Pages");
         System.out.println("Total of pages printed is: " + printer.getPagesPrinted());
         System.out.println("Toner level is " + printer.getTonerLevel());
+        printer.addToner(50);
+        System.out.println("Toner level is " + printer.getTonerLevel());
+
+        int pagesPrinted = printer.printPages(54);
+        System.out.printf("Current pages printed:%d %nPrinter total pages: %d%n",pagesPrinted,printer.getPagesPrinted());
+         pagesPrinted = printer.printPages(19);
+        System.out.printf("Current pages printed:%d %nPrinter total pages: %d%n",pagesPrinted,printer.getPagesPrinted());
+
+
 
     }
 }
--- src/Encapsulation/EncapsulationChallenge/Printer.java
+++ src/Encapsulation/EncapsulationChallenge/Printer.java
@@ -6,30 +6,23 @@ public class Printer {
     private boolean duplex;
 
     public Printer(int tonerLevel, boolean duplex) {
-        if(tonerLevel<0)tonerLevel=1;
-        else if(tonerLevel >100)tonerLevel=100;
-        this.tonerLevel = tonerLevel;
+        this.tonerLevel = addToner(tonerLevel);
         this.duplex = duplex;
     }
 
-    public int addToner(int tonerAmount){
-        if(tonerLevel+tonerAmount<0 || tonerLevel+ tonerAmount>100)return -1;
-        return tonerLevel+=tonerAmount;
+    public int addToner(int tonerAmount) {
+        if (tonerLevel + tonerAmount < 0 || tonerLevel + tonerAmount > 100) return -1;
+        return tonerLevel += tonerAmount;
     }
 
-    public int printPages(int pagesToBePrinted){
-        if(pagesToBePrinted<0){
+    public int printPages(int pagesToBePrinted) {
+        if (pagesToBePrinted < 0) {
             System.out.println("Number of pages not valid!");
             return -1;
         }
-        if(duplex){
-            System.out.println("It's a duplex");
-            pagesPrinted+= 2*pagesToBePrinted;
-            return 2*pagesToBePrinted;
-        }else{
-            pagesPrinted+= pagesToBePrinted;
-            return pagesToBePrinted;
-        }
+        int pages = (duplex) ? (pagesToBePrinted / 2) + (pagesToBePrinted % 2) : pagesToBePrinted;
+        pagesPrinted+=pages;
+        return pages;
     }
 
     public int getTonerLevel() {
