0602e65ba60ddf5aa81b339537124fa433c52ff1
GIL32610 <gil.00000032610@unicap.br>
2024-02-28 11:07:23 -0300

    Adjustments on overridden methods

--- src/Polymorphism/PolymorphismChallenge/CarClass/ElectricCar.java
+++ src/Polymorphism/PolymorphismChallenge/CarClass/ElectricCar.java
@@ -14,6 +14,8 @@ public class ElectricCar extends Car{
     public void startEngine() {
         super.startEngine();
         System.out.println("Battery is now powering the motor");
+        System.out.printf("Power consumption is under the expected average: %.2f%n",avgKmPerCharge);
+        System.out.println();
     }
 
     @Override
--- src/Polymorphism/PolymorphismChallenge/Main.java
+++ src/Polymorphism/PolymorphismChallenge/Main.java
@@ -7,20 +7,18 @@ import Polymorphism.PolymorphismChallenge.CarClass.HybridCar;
 public class Main {
     public static void main(String[] args) {
         Car car = new Car("Red car");
+        Car hybridCar = new HybridCar("Porsche",10.0,5000,12);
+        Car gasPoweredCar = new GasPoweredCar("Ferrari", 6,12);
+        Car[] cars = {hybridCar,gasPoweredCar,car};
+        for(Car vehicle: cars){
+            runRace(vehicle);
+        }
+    }
+
+    public static void runRace(Car car){
         car.drive();
         car.startEngine();
         car.drive();
-
-        HybridCar hybridCar = new HybridCar("Porsche",10.0,5000,12);
-        hybridCar.drive();
-        hybridCar.startEngine();
-        hybridCar.drive();
-        hybridCar.shutEngineOff();
-
-        GasPoweredCar gasPoweredCar = new GasPoweredCar("Ferrari", 6,12);
-        gasPoweredCar.drive();
-        gasPoweredCar.startEngine();
-        gasPoweredCar.drive();
-        gasPoweredCar.shutEngineOff();
+        car.shutEngineOff();
     }
 }
