3142bc4b86abf8b6e06141ecfbd40b6178e4df27
GIL32610 <gil.00000032610@unicap.br>
2024-03-04 15:04:43 -0300

    Package hierarchy and Import statement

---
+++ src/Packages/dev/lpa/Main.java
@@ -0,0 +1,13 @@
+package Packages.dev.lpa;
+
+import com.abc.first.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        com.abc.first.Item item = new Item("Topping");
+        System.out.println(item);
+    }
+
+
+}
---
+++ src/com/abc/first/Item.java
@@ -0,0 +1,16 @@
+package com.abc.first;
+
+public class Item {
+    private String type;
+
+    public Item(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "type='" + type + '\'' +
+                '}';
+    }
+}
